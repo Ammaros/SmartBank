7bc619d420853489d47bc4227bcdcca57cb43972
Ammaros <ammarstudent2000@gmail.com>
2020-07-26 22:56:16 +0500

    Transaction History

--- bankapp/models.py
+++ bankapp/models.py
@@ -1,5 +1,39 @@
+import ast
 from django.db import models
 from django.contrib.auth.forms import User
+
+class ListField(models.TextField):
+	description = "Stores a python list"
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	def from_db_value(self, value, expression, connection):
+		if value is None:
+			return value
+		if isinstance(value, str):
+			return value.split(',')
+
+	def to_python(self, value):
+		if not value:
+			value = []
+		if isinstance(value, list):
+			return value
+		if isinstance(value, str):
+			return ast.literal_eval(value)
+
+	def get_prep_value(self, value):
+		if value is None:
+			return value
+		if value is not None and isinstance(value, str):
+			return value
+		if isinstance(value, list):
+			return ','.join(value)
+
+	def value_to_string(self, obj):
+		value = self.value_from_object(obj)
+		return self.get_prep_value(value)
+
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,6 +41,7 @@ class Account(models.Model):
     balance = models.IntegerField()
     transactions = models.IntegerField(null=False, blank=True, default=0)
     balanceSpent = models.IntegerField(null=False, blank=True, default=0)
+    history = ListField(null=False, blank=True)
 
     def __str__(self):
         return self.user.username
--- bankapp/views.py
+++ bankapp/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, authenticate, login
 from bankapp.forms import SignUpForm, AccountForm
@@ -26,10 +27,14 @@ def signup_view(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
 
+            user = User.objects.get(username=request.user.username)
+            user.account.history = user.account.history.remove('')
+            user.account.save()
+
             messages.info(request, "Account Created Sucessfully! Login.")
             return redirect('login-page')
         else:
-           messages.error(request, 'Invalid input or missing fields. Please try again!')
+           messages.warning(request, 'Invalid input or missing fields. Please try again!')
            return redirect('signup-page') 
     else:
         form = SignUpForm()
@@ -51,10 +56,10 @@ def login_view(request):
                 login(request, user)
                 return redirect('dashboard')
             else:
-                messages.error(request,'Invalid login credentials. Please try agian.')
+                messages.warning(request,'Invalid login credentials. Please try agian.')
                 return redirect('login-page')
         else:
-            messages.error(request,'Invalid login credentials. Please try agian.')
+            messages.warning(request,'Invalid login credentials. Please try agian.')
             return redirect('login-page')
 
     form = AuthenticationForm()
@@ -63,8 +68,8 @@ def login_view(request):
 def logout_view(request):
 
     logout(request)
-    messages.info(request, "Logged out successfully!")
 
+    messages.info(request, "Logged out successfully!")
     return redirect("login-page")
 
 @login_required(redirect_field_name='login-page')
@@ -74,8 +79,6 @@ def dashboard(request):
         withdrawAmount = request.POST.get('withdraw')
         depositAmount = request.POST.get('deposit')
         user = User.objects.get(username=request.user.username)
-        print(withdrawAmount, depositAmount)
-
 
         try:
 
@@ -83,35 +86,41 @@ def dashboard(request):
 
                 if user.account.balance >= int(withdrawAmount):
 
+                    date = datetime.date.today()
+
                     user.account.balance = user.account.balance - int(withdrawAmount)
                     user.account.balanceSpent += int(withdrawAmount)
                     user.account.transactions += 1
+                    user.account.history.append(f'{date}: Withdrew Rs. {withdrawAmount}')
                     user.account.save()
                     return redirect('dashboard')
                 
                 else:
-
+                    
+                    messages.warning(request, 'Withdraw amount exceeds current balance.')
                     return redirect('dashboard')
-                    messages.error(request, 'Withdraw amount exceeds current balance.')
 
             elif withdrawAmount == None:
 
                 if int(depositAmount) <= 100000:
+
+                    date = datetime.date.today()
                     
                     user.account.balance = user.account.balance + int(depositAmount)
                     user.account.transactions += 1
+                    user.account.history.append(f'{date}: Deposited Rs. {depositAmount}')
                     user.account.save()
                     return redirect('dashboard')
                 
                 else:
 
+                    messages.warning(request, 'Can only deposit a maximum of Rs. 100,000')
                     return redirect('dashboard')
-                    messages.error(request, 'Can only deposit a mzximum of Rs. 100,000')
         
         except ValueError:
 
+            messages.warning(request, 'Input Valid Amount')
             return redirect('dashboard')
-            messages.error(request, 'Invalid Input! Please try again')
 
             
     customer = User.objects.all()
@@ -123,44 +132,59 @@ def transfer(request):
 
         destUsername = request.POST.get('accountUsername')
         transferAmount = request.POST.get('amount')
-        user = User.objects.get(username=request.user.username)
-        transferUser = User.objects.get(username=destUsername)
 
         try:
 
-            if destUsername != '' or transferAmount != None:
+            user = User.objects.get(username=request.user.username)
+            transferUser = User.objects.get(username=destUsername)
+
+        except User.DoesNotExist:
+
+            messages.warning(request, 'User does not exist!')
+            return redirect('transfer')
+
+        try:
 
                 if user.account.balance >= int(transferAmount) and int(transferAmount) <= 100000:
+
+                    date = datetime.date.today()
 
                     user.account.balance = user.account.balance - int(transferAmount)
                     user.account.balanceSpent += int(transferAmount)
                     user.account.transactions += 1
+                    user.account.history.append(f'{date}: Transferred Rs. {transferAmount} to {transferUser}')
+                
                     transferUser.account.balance = transferUser.account.balance + int(transferAmount)
+                    transferUser.account.transactions += 1
+                    transferUser.account.history.append(f'{date}: Recieved Rs. {transferAmount} from {user}')
 
                     user.account.save()
                     transferUser.account.save()
 
-                    return redirect('transfer')
                     messages.success(request, 'Funds transferred successfully!')
+                    return redirect('transfer')
             
                 else:
 
+                    messages.warning(request, 'Amount out of bounds!')
                     return redirect('transfer')
-                    messages.error(request, 'Please complete the input fields!')
 
-            else:
+        except ValueError:
 
-                return redirect('transfer')
-                messages.error(request, 'Please complete the input fields!')
-
-        except transferUser.DoesNotExist or ValueError:
-
+            messages.warning(request, 'Input Valid Amount!')
             return redirect('transfer')
-            messages.error(request, 'Invalid input a valid Username or Amount!')
 
     customer = User.objects.all()
     return render(request, 'bankapp/transfer.html', {'customer': customer})
 
 @login_required(redirect_field_name='login-page')
 def transactions(request):
-    return render(request, 'bankapp/transactions.html')
+    user = User.objects.get(username=request.user.username)
+
+    if len(user.account.history) > 10:
+        sliceValue = len(user.account.history) - 10
+        user.account.history = user.account.history[sliceValue:]
+        user.account.save()
+
+    customer = User.objects.all()
+    return render(request, 'bankapp/transactions.html', {'customer': customer})
